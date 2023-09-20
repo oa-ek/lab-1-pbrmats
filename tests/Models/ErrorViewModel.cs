284a08906c8c2e2ce486fabc0b44af8fb09dfdbf
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2023-09-20 21:51:34 +0300

    Deleted old projects

--- tests/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace tests.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
