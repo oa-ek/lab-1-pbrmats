6b165aae8d3ca65e16649720e8d2fb88cb2d6bbd
narberalAntares <valerii.deineka.volodymyrovych@gmail.com>
2023-12-11 15:45:09 +0200

    Reworked LicenseController

--- PBRmatsWeb/Controllers/LicenseController.cs
+++ PBRmatsWeb/Controllers/LicenseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PBRmats.Application.DTOs;
 using PBRmats.Core.Entities;
 using PBRmats.Repositories.Interfaces;
 
@@ -10,28 +11,43 @@ namespace PBRmatsWeb.Controllers
     {
         private readonly IRepository<License, int> _licenseRepository;
 
-        public LicenseController(IRepository<License, int> licenseRepository)
-        {
+        public LicenseController(IRepository<License, int> licenseRepository) => 
             _licenseRepository = licenseRepository;
+
+        private License GetLicense(int id) =>
+            _licenseRepository.Get(id);
+
+        private LicenseDTO SetDTO(int id)
+        {
+            var categoryEntity = GetLicense(id);
+
+            return new LicenseDTO() { Id = categoryEntity.Id, Title = categoryEntity.Title };
         }
 
-        public IActionResult Index()
-        {
-            return View(_licenseRepository.GetAll());
-        }
+        public IActionResult Index() =>
+            View(_licenseRepository.GetAll());
 
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult Create() =>
+            View("Create");
+
+        public IActionResult Edit(int id)
         {
-            return View("Create");
+            return View(SetDTO(id));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(SetDTO(id));
         }
 
         [HttpPost]
-        public IActionResult Create(License license)
+        public IActionResult Create(LicenseDTO license)
         {
             if (ModelState.IsValid)
             {
-                _licenseRepository.Create(license);
+                var newLicense = new License() { Title = license.Title };
+                _licenseRepository.Create(newLicense);
 
                 return RedirectToAction("Index");
             }
@@ -39,28 +55,21 @@ namespace PBRmatsWeb.Controllers
             return View();
         }
 
-        public IActionResult Delete(int id)
-        {
-            return View(_licenseRepository.Get(id));
-        }
-
         [HttpPost]
-        public IActionResult Delete(License license)
+        public IActionResult Edit(LicenseDTO license)
         {
-            _licenseRepository.Delete(license);
+            var licenseToEdit = GetLicense(license.Id);
+            licenseToEdit.Title = license.Title;
+            _licenseRepository.Update(licenseToEdit);
 
             return RedirectToAction("Index");
         }
 
-        public IActionResult Edit(int id)
-        {
-            return View(_licenseRepository.Get(id));
-        }
-
         [HttpPost]
-        public IActionResult Edit(License license)
+        public IActionResult Delete(LicenseDTO license)
         {
-            _licenseRepository.Update(license);
+            var licenseToDelete = GetLicense(license.Id);
+            _licenseRepository.Delete(licenseToDelete);
 
             return RedirectToAction("Index");
         }
